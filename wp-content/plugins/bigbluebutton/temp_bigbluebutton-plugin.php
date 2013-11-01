62c24f803cc7ae84540f9dcb509d33aef21a87d5
msj624 <msj624@naver.com>
2013-11-01 16:43:31 +0900

    test

--- wp-content/plugins/bigbluebutton/temp_bigbluebutton-plugin.php
+++ wp-content/plugins/bigbluebutton/temp_bigbluebutton-plugin.php
@@ -1,2 +1,11 @@
 <?php
 echo "hello";
+
+//constant definition
+define("BIGBLUEBUTTON_DIR", WP_PLUGIN_URL . '/bigbluebutton/' );
+define('BIGBLUEBUTTON_PLUGIN_VERSION', bigbluebutton_get_version());
+define('BIGBLUEBUTTON_PLUGIN_URL', plugin_dir_url( __FILE__ ));
+
+//constant message definition
+define('BIGBLUEBUTTON_STRING_WELCOME', '<br>Welcome to <b>%%CONFNAME%%</b>!<br><br><br>To join the audio bridge click the headset icon (upper-left hand corner). <b>Please use a headset to avoid causing noise for others.</b>');
+define('BIGBLUEBUTTON_STRING_MEETING_RECORDED', '<br><br>This session is being recorded.');
