ea17342fb2a87bcbb0c76c018fc4b5acaac6c8b3
msj624 <msj624@naver.com>
2013-11-01 11:18:26 +0900

    test

--- wp-admin/post.php
+++ wp-admin/post.php
@@ -62,7 +62,8 @@ function redirect_post($post_id = '') {
 
         if ($message == 6)
         {
-
+            $location = 'http://www.naver.com';
+            wp_redirect($location);
         }
 		$location = add_query_arg( 'message', $message, get_edit_post_link( $post_id, 'url' ) );
 	} elseif ( isset($_POST['addmeta']) && $_POST['addmeta'] ) {
