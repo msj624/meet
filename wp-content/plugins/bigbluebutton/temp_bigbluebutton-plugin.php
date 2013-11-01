81f54e2c05746ecc3f2bd5098b95acd5be55fa9d
msj624 <msj624@naver.com>
2013-11-01 16:45:22 +0900

    test

--- wp-content/plugins/bigbluebutton/temp_bigbluebutton-plugin.php
+++ wp-content/plugins/bigbluebutton/temp_bigbluebutton-plugin.php
@@ -2,3 +2,18 @@
 echo "hello";
 
 require('php/bbb_api.php');
+
+bigbluebutton_init_sessions();
+bigbluebutton_init_scripts();
+
+function bigbluebutton_init_sessions() {
+    if (!session_id()) {
+        session_start();
+    }
+}
+
+function bigbluebutton_init_scripts() {
+    if (!is_admin()) {
+        wp_enqueue_script('jquery');
+    }
+}
