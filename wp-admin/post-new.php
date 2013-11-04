c830b27a4e5a30bf99074d2ea9e61845abd3db72
msj624 <msj624@naver.com>
2013-11-04 13:36:48 +0900

    test

--- wp-admin/post-new.php
+++ wp-admin/post-new.php
@@ -78,7 +78,7 @@ function my_default_content( $post_content ) {
     <a href='.$_POST['join'].' onclick="javascript:return validatePass()">[Creator Join Meeting] enter starting password and click this</a>
     <script>
     function validatePass(){
-        if(document.getElementById(\'password\').value == '.$_POST['password'].'){
+        if(document.getElementById(\'password\').value == \''.$_POST['password'].'\'){
             return true;
         }else{
             alert(\'wrong password!!\');
