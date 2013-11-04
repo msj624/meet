ec5fc952286acc81c6235a21896d069dbcc29739
msj624 <msj624@naver.com>
2013-11-04 16:15:11 +0900

    test

--- wp-admin/post-new.php
+++ wp-admin/post-new.php
@@ -74,9 +74,21 @@ if ('event' == $_POST['post_type'])
     add_filter( 'default_content' , 'my_default_content' );
     function my_default_content( $post_content ) {
 
+    //<a title="Join" href="'.$_POST['invite'].'">Join Meeting (Guest)</a><br><br>
+
     $post_content = 'Enter the meeting description.<br><br>
 
-    <a title="Join" href="'.$_POST['invite'].'">Join Meeting (Guest)</a><br><br>
+    <script>
+    function post(event) {
+        event.preventDefault();
+        document.getElementById("my_form").submit();
+    }
+    </script>
+
+    <form action="goJoin.php" method="post" id="my_form" style="display: none;">
+        <input type="hidden" name="invite" value='.$_POST['invite'].'/>
+    </form>
+    <a href="#" onclick="post()">Join Meeting (Guest)</a>
 
     Enter password (Creator): <input id=\'password1\' type=\'password\'/><br><br>
 
