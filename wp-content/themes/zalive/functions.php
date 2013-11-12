716672567e54c686523bf4779cb7551ebe2656b7
msj624 <msj624@naver.com>
2013-11-12 11:01:52 +0900

    test

--- wp-content/themes/zalive/functions.php
+++ wp-content/themes/zalive/functions.php
@@ -1021,9 +1021,9 @@
         return $location;
     }
 
-    //add_action( 'admin_print_styles', 'custom_admin_css' );
+    add_action( 'admin_print_styles', 'custom_admin_css' );
     //add_action( 'wp_enqueue_scripts', 'custom_admin_css' );
-    add_action('admin_init', 'custom_admin_css');
+    //add_action('admin_init', 'custom_admin_css');
 
     function custom_admin_css(){
 
