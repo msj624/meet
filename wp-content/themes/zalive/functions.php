451a29406a17e997c8c54eea4f93d975c2fe8c74
msj624 <msj624@naver.com>
2013-11-12 14:48:33 +0900

    beta2

--- wp-content/themes/zalive/functions.php
+++ wp-content/themes/zalive/functions.php
@@ -1028,6 +1028,7 @@
     function custom_admin_css(){
 
         if( is_admin() && !current_user_can('administrator')) {
+            wp_enqueue_style("custom_admin_css_nav", get_bloginfo('template_directory')."/css/custom_admin_nav.css", false, false, "all");
             wp_enqueue_style("custom_admin_css", get_bloginfo('template_directory')."/css/custom_admin.css", false, false, "all");
         }
     }
@@ -1035,7 +1036,7 @@
     add_action('admin_head', 'nav_script_enqueuer');
     function nav_script_enqueuer(){
         get_template_part( 'template/header_kt' );
-        wp_enqueue_style("custom_admin_css_nav", get_bloginfo('template_directory')."/css/custom_admin_nav.css", false, false, "all");
+
     }
 ?>
 
