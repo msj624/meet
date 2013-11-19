7ece266717f99599f6fe904a4db50fb6e9c5c59f
msj624 <msj624@naver.com>
2013-11-19 12:07:24 +0900

    beta2

--- wp-content/plugins/events-manager/admin/bookings/em-events.php
+++ wp-content/plugins/events-manager/admin/bookings/em-events.php
@@ -1,21 +1,21 @@
 <?php
 
 /**
- * Determines whether to show event page or events page, and saves any updates to the event or events
+ * Determines whether to show meeting page or meetings page, and saves any updates to the meeting or meetings
  * @return null
  */
-function em_bookings_events_table() {
-	//TODO Simplify panel for events, use form flags to detect certain actions (e.g. submitted, etc)
+function em_bookings_meetings_table() {
+	//TODO Simplify panel for meetings, use form flags to detect certain actions (e.g. submitted, etc)
 	global $wpdb;
-	global $EM_Event;
+	global $EM_Meeting;
 
 	$scope_names = array (
-		'past' => __ ( 'Past events', 'dbem' ),
-		'all' => __ ( 'All events', 'dbem' ),
-		'future' => __ ( 'Future events', 'dbem' )
+		'past' => __ ( 'Past meetings', 'dbem' ),
+		'all' => __ ( 'All meetings', 'dbem' ),
+		'future' => __ ( 'Future meetings', 'dbem' )
 	);
 	
-	$action_scope = ( !empty($_REQUEST['em_obj']) && $_REQUEST['em_obj'] == 'em_bookings_events_table' );
+	$action_scope = ( !empty($_REQUEST['em_obj']) && $_REQUEST['em_obj'] == 'em_bookings_meetings_table' );
 	$action = ( $action_scope && !empty($_GET ['action']) ) ? $_GET ['action']:'';
 	$order = ( $action_scope && !empty($_GET ['order']) ) ? $_GET ['order']:'ASC';
 	$limit = ( $action_scope && !empty($_GET['limit']) ) ? $_GET['limit'] : 20;//Default limit
@@ -23,36 +23,36 @@ function em_bookings_events_table() {
 	$offset = ( $action_scope && $page > 1 ) ? ($page-1)*$limit : 0;
 	$scope = ( $action_scope && !empty($_GET ['scope']) && array_key_exists($_GET ['scope'], $scope_names) ) ? $_GET ['scope']:'future';
 	
-	// No action, only showing the events list
+	// No action, only showing the meetings list
 	switch ($scope) {
 		case "past" :
-			$title = __ ( 'Past Events', 'dbem' );
+			$title = __ ( 'Past Meetings', 'dbem' );
 			break;
 		case "all" :
-			$title = __ ( 'All Events', 'dbem' );
+			$title = __ ( 'All Meetings', 'dbem' );
 			break;
 		default :
-			$title = __ ( 'Future Events', 'dbem' );
+			$title = __ ( 'Future Meetings', 'dbem' );
 			$scope = "future";
 	}
 	$owner = !current_user_can('manage_others_bookings') ? get_current_user_id() : false;
-	$events = EM_Events::get( array('scope'=>$scope, 'limit'=>$limit, 'offset' => $offset, 'order'=>$order, 'bookings'=>true, 'owner' => $owner ) );
-	$events_count = EM_Events::count( array('scope'=>$scope, 'limit'=>0, 'order'=>$order, 'bookings'=>true, 'owner' => $owner ) );
+	$meetings = EM_Meetings::get( array('scope'=>$scope, 'limit'=>$limit, 'offset' => $offset, 'order'=>$order, 'bookings'=>true, 'owner' => $owner ) );
+	$meetings_count = EM_Meetings::count( array('scope'=>$scope, 'limit'=>0, 'order'=>$order, 'bookings'=>true, 'owner' => $owner ) );
 	
-	$use_events_end = get_option ( 'dbem_use_event_end' );
+	$use_meetings_end = get_option ( 'dbem_use_meeting_end' );
 	?>
-	<div class="wrap em_bookings_events_table em_obj">
+	<div class="wrap em_bookings_meetings_table em_obj">
 		<form id="posts-filter" action="" method="get">
-			<input type="hidden" name="em_obj" value="em_bookings_events_table" />
+			<input type="hidden" name="em_obj" value="em_bookings_meetings_table" />
 			<?php if(!empty($_GET['page'])): ?>
-			<input type='hidden' name='page' value='events-manager-bookings' />
+			<input type='hidden' name='page' value='meetings-manager-bookings' />
 			<?php endif; ?>		
 			<div class="tablenav">			
 				<div class="alignleft actions">
 					<!--
 					<select name="action">
 						<option value="-1" selected="selected"><?php esc_html_e( 'Bulk Actions' ); ?></option>
-						<option value="deleteEvents"><?php esc_html_e( 'Delete selected','dbem' ); ?></option>
+						<option value="deleteMeetings"><?php esc_html_e( 'Delete selected','dbem' ); ?></option>
 					</select> 
 					<input type="submit" value="<?php esc_html_e( 'Apply' ); ?>" name="doaction2" id="doaction2" class="button-secondary action" />
 					 --> 
@@ -74,53 +74,53 @@ function em_bookings_events_table() {
 				</div>
 				-->
 				<?php 
-				if ( $events_count >= $limit ) {
-					$events_nav = em_admin_paginate( $events_count, $limit, $page, array('em_ajax'=>0, 'em_obj'=>'em_bookings_events_table'));
-					echo $events_nav;
+				if ( $meetings_count >= $limit ) {
+					$meetings_nav = em_admin_paginate( $meetings_count, $limit, $page, array('em_ajax'=>0, 'em_obj'=>'em_bookings_meetings_table'));
+					echo $meetings_nav;
 				}
 				?>
 			</div>
 				
 			<?php
-			if (empty ( $events )) {
+			if (empty ( $meetings )) {
 				// TODO localize
-				_e ( 'no events','dbem' );
+				_e ( 'no meetings','dbem' );
 			} else {
 			?>
 			<div class='table-wrap'>	
 			<table class="widefat">
 				<thead>
 					<tr>
-						<th><?php esc_html_e( 'Event', 'dbem' ); ?></th>
+						<th><?php esc_html_e( 'Meeting', 'dbem' ); ?></th>
 						<th><?php esc_html_e( 'Date and time', 'dbem' ); ?></th>
 					</tr>
 				</thead>
 				<tbody>
 					<?php 
 					$rowno = 0;
-					foreach ( $events as $event ) {
-						/* @var $event EM_Event */
+					foreach ( $meetings as $meeting ) {
+						/* @var $meeting EM_Meeting */
 						$rowno++;
 						$class = ($rowno % 2) ? ' class="alternate"' : '';
 						// FIXME set to american
-						$localised_start_date = date_i18n(get_option('date_format'), $event->start);
-						$localised_end_date = date_i18n(get_option('date_format'), $event->end);
+						$localised_start_date = date_i18n(get_option('date_format'), $meeting->start);
+						$localised_end_date = date_i18n(get_option('date_format'), $meeting->end);
 						$style = "";
 						$today = date ( "Y-m-d" );
 						
-						if ($event->start_date < $today && $event->end_date < $today){
+						if ($meeting->start_date < $today && $meeting->end_date < $today){
 							$style = "style ='background-color: #FADDB7;'";
 						}							
 						?>
 						<tr <?php echo "$class $style"; ?>>
 							<td>
 								<strong>
-									<?php echo $event->output('#_BOOKINGSLINK'); ?>
+									<?php echo $meeting->output('#_BOOKINGSLINK'); ?>
 								</strong>
 								&ndash; 
-								<?php esc_html_e("Booked Spaces",'dbem') ?>: <?php echo $event->get_bookings()->get_booked_spaces()."/".$event->get_spaces() ?>
+								<?php esc_html_e("Booked Spaces",'dbem') ?>: <?php echo $meeting->get_bookings()->get_booked_spaces()."/".$meeting->get_spaces() ?>
 								<?php if( get_option('dbem_bookings_approval') == 1 ) : ?>
-									| <?php esc_html_e("Pending",'dbem') ?>: <?php echo $event->get_bookings()->get_pending_spaces(); ?>
+									| <?php esc_html_e("Pending",'dbem') ?>: <?php echo $meeting->get_bookings()->get_pending_spaces(); ?>
 								<?php endif; ?>
 							</td>
 					
@@ -129,8 +129,8 @@ function em_bookings_events_table() {
 								<?php echo ($localised_end_date != $localised_start_date) ? " - $localised_end_date":'' ?>
 								&ndash;
 								<?php
-									//TODO Should 00:00 - 00:00 be treated as an all day event? 
-									echo substr ( $event->start_time, 0, 5 ) . " - " . substr ( $event->end_time, 0, 5 ); 
+									//TODO Should 00:00 - 00:00 be treated as an all day meeting? 
+									echo substr ( $meeting->start_time, 0, 5 ) . " - " . substr ( $meeting->end_time, 0, 5 ); 
 								?>
 							</td>
 						</tr>
@@ -147,10 +147,10 @@ function em_bookings_events_table() {
 				<div class="alignleft actions">
 				<br class='clear' />
 				</div>
-				<?php if (!empty($events_nav) &&  $events_count >= $limit ) : ?>
+				<?php if (!empty($meetings_nav) &&  $meetings_count >= $limit ) : ?>
 				<div class="tablenav-pages">
 					<?php
-					echo $events_nav;
+					echo $meetings_nav;
 					?>
 				</div>
 				<?php endif; ?>
