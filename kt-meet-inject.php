b9cda2343f4f8c5a939014e5698a0e69e5775001
msj624 <msj624@naver.com>
2013-11-20 17:19:42 +0900

    language

--- kt-meet-inject.php
+++ kt-meet-inject.php
@@ -32,10 +32,10 @@ if ( is_user_logged_in() ) {
     $event_mader = $current_user->ID ;
 
     $post_contents = $post_contents.
-        '<form  action="redirect-to-meet.php" method="post">
+        '<form  id="my_form" action="redirect-to-meet.php" method="post">
         <input type="hidden" value="'.$meetingID.'" name="meetingID" />
         <input type="hidden" value="'.$event_mader.'" name="mader" />
-        <a id="joinbutton"></a>
+        <a id="joinbutton" onclick="document.getElementById("my_form").submit(); return false;"></a>
         </form>';
 
     $my_post = array();
