3da23edfa29fcb4493b9344cee77dcf8dd6d2fca
msj624 <msj624@naver.com>
2014-01-14 10:16:03 +0900

    name

--- kt-meet-inject.php
+++ kt-meet-inject.php
@@ -33,7 +33,7 @@ if ( is_user_logged_in() ) {
 
     $post_contents = $post_contents.
         '<p><img style="padding: 5px; vertical-align: middle;" src="http://183.110.207.46/wp/wp-content/plugins/email-users/images/email.png" <="" img=""><a href="http://183.110.207.46/wp/wp-admin/admin.php?page=mailusers-send-notify-mail-post&post_id='.$post_id.'">Notify Users About this Meeting</a></p>
-        <form  action="redirect-to-meet.php" method="post"><input type="hidden" value="'.$meetingID.'" name="meetingID" /><input type="hidden" value="'.$event_mader.'" name="mader" /><button id="joinbutton" name="redirect_submit" value="redirect"/></form>';
+        <form  action="redirect-to-meet.php" method="post"><input type="hidden" value="'.$meetingID.'" name="meetingID" /><input type="hidden" value="'.$event_mader.'" name="mader" /><button id="joinbutton" name="redirect_submit" value="redirect"></button></form>';
 
     $my_post = array();
     $my_post['ID'] = $post_id;
